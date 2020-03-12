11574d9d3d17dd40135250d26722bcd44605a80f
Lola Ozoh <lollydeeo@yahoo.com>
2020-03-12 13:27:02 +0100

    This is the initial solution to the coding challenge The application  lists all the tables within the database alongside other table information        The application  includes a user registration and login system        User is  able to select tables for dumping with a check        Ability to dump the whole database  exists        The application  takes database connection credentials        The application  shows database backup/dump history based on logged-in user        The application include options that enables the user download the dumps to their local machine or save to their Google Drive

--- app/Http/Controllers/dbaselist.php
+++ app/Http/Controllers/dbaselist.php
@@ -30,34 +30,18 @@ class dbaselist extends Controller
     {
         if (Auth::user()){
 
-            //$tables = DB::select('SHOW TABLES'); // returns an array of stdObjects
-
-           //$dbh = Schema::getColumnListing($tables);
-
-          //$dblist = new PDO('mysql:host=localhost;user=dbuser;password=Db@12345;');
-           // $statement = $dblist->query('SHOW DATABASES');
-          // $columns= $statement->fetchAll(PDO::FETCH_COLUMN);
+           //get all sent variables
             $username = $request->username;
             $password = $request->password;
             $database = $request->dbname;
             $host = $request->hostname;
             $mysqli = new mysqli("localhost", $username, $password, $database);
             //connect to database and exit if not valid
-
+//select all tables from the database
             $schema = DB::table('information_schema.TABLES')
                 ->select('table_collation', 'table_collation','engine','data_length','index_length','update_time','create_time')
                 ->where('table_schema', '=',$request->dbname )
                 ->get();
-            //$connection = mysqli_connect('localhost','dbuser','Db@12345','databasedump');
-           //$result = $mysqli->query($schema);
-
-            //$pdo = new PDO("mysql:host=$host;port=$port", $user, $password);
-
-//Execute a "SHOW DATABASES" SQL query.
-           // $stmt = $pdo->query('SHOW DATABASES');
-
-//Fetch the columns from the returned PDOStatement
-            //$databases = $stmt->fetchAll(PDO::FETCH_COLUMN);
 
 
         }
@@ -71,13 +55,7 @@ class dbaselist extends Controller
         $user =  User::where('name','=',Auth::user()->name)->first();
         if (Auth::user()){
 
-            //$tables = DB::select('SHOW TABLES'); // returns an array of stdObjects
 
-            //$dbh = Schema::getColumnListing($tables);
-
-            //$dblist = new PDO('mysql:host=localhost;user=dbuser;password=Db@12345;');
-            // $statement = $dblist->query('SHOW DATABASES');
-            // $columns= $statement->fetchAll(PDO::FETCH_COLUMN);
             $username = $request->username;
             $password = $request->password;
             $dbs = $request->mydbase;
@@ -89,9 +67,7 @@ class dbaselist extends Controller
                 ->select('table_schema','table_collation','table_name', 'table_rows','engine','data_length','index_length','update_time','create_time')
                 ->where('table_schema', '=',$request->mydbase )
                 ->get();
-            //$connection = mysqli_connect('localhost','dbuser','Db@12345','databasedump'); DB::table ('programme_courses', 'programme_courses.progid', '=', $student->progid)
-            //$result = $mysqli->query($schema);,'tables.table_schema', '=',$request->mydbase
-            //$connection = mysqli_connect('localhost',$username, $password, $dbs);
+           //fetch all tables
             $tables = array();
             $result = $mysqli->query("SHOW TABLES");
 
--- app/Http/Controllers/dumpfiles.php
+++ app/Http/Controllers/dumpfiles.php
@@ -32,44 +32,7 @@ class dumpfiles extends Controller
      */
     public function index(Request $request)
     {
-        //if (Auth::user()){
 
-            //$tables = DB::select('SHOW TABLES'); // returns an array of stdObjects
-
-           //$dbh = Schema::getColumnListing($tables);
-
-
-           // $statement = $dblist->query('SHOW DATABASES');
-          // $columns= $statement->fetchAll(PDO::FETCH_COLUMN);
-           /* $username = $request->username;
-            $password = $request->password;
-            $database = $request->dbname;
-            $host = $request->hostname;
-            $dbs =  $request->dbs;
-            $allmytable =  $request->allmytable;
-            $mytable =  $request->mytable;
-
-            $mysqli = new mysqli("localhost", $username, $password, $database);
-            //connect to database and exit if not valid
-$msg="dump complete";
-            $schema = DB::table('information_schema.TABLES')
-                ->select('table_collation', 'table_collation','engine','data_length','index_length','update_time','create_time')
-                ->where('table_schema', '=',$request->dbname )
-                ->get();
-            //$connection = mysqli_connect('localhost','dbuser','Db@12345','databasedump');
-           //$result = $mysqli->query($schema);
-            exec('mysqldump --user=$username --password=$password --host=$host $dbs > /c:/downloads/file.sql');
-            //$pdo = new PDO("mysql:host=$host;port=$port", $user, $password);
-
-//Execute a "SHOW DATABASES" SQL query.
-           // $stmt = $pdo->query('SHOW DATABASES');
-
-//Fetch the columns from the returned PDOStatement
-            //$databases = $stmt->fetchAll(PDO::FETCH_COLUMN);
-
-
-        }
-        return view('dumpfiles',compact('msg','allmytable','mytable','dbs'));*/
     }
     public function show(Request $request){
 
@@ -78,7 +41,7 @@ $msg="dump complete";
         if (Auth::user()){
 
             $dbs = $request->dbs;
-
+//get info for dump logs
             $schema = DB::table('dumplogs')
                 ->select('username','dumpsite','database', 'mytables','created_at','type')
                 ->where('database', '=',$request->dbs )
@@ -95,13 +58,7 @@ $msg="dump complete";
         $user =  User::where('name','=',Auth::user()->name)->first();
         if (Auth::user()){
 
-            //$tables = DB::select('SHOW TABLES'); // returns an array of stdObjects
 
-            //$dbh = Schema::getColumnListing($tables);
-
-            //$dblist = new PDO('mysql:host=localhost;user=dbuser;password=Db@12345;');
-            // $statement = $dblist->query('SHOW DATABASES');
-            // $columns= $statement->fetchAll(PDO::FETCH_COLUMN);
             $username = $request->username;
             $password = $request->password;
             $dbs = $request->dbs;
@@ -122,36 +79,7 @@ $msg="dump complete";
             // $command = "\"C:\\xampp\\mysql\\bin\\mysqldump.exe\" --opt --skip-extended-insert --complete-insert -h ".$host." -user ".$username." -password".$password." ".$dbs." > backup.sql";;
 
 
-            /*$backupFile = "backup" . date("Y_m_d") . '.sql';
-            $command = "mysqldump --opt -h $host -u $username -p $password $dbs > $backupFile";
-            system($command , $return);
 
-            echo "Returned: ".$return;*/
-
-            /*  $cmd =
-                  "mysqldump -h " . $host .
-                  " -u "          . $username .
-                  " -p"         . $password .
-                  " " . $dbs;
-
-              echo "Returned: ".$cmd;
-              $output = [];
-              $name = 'dbasedumps' . date('Y-m-d H-i') . '.sql';
-
-              exec($cmd, $output);
-
-              $tmppath = tempnam(sys_get_temp_dir(), $name);
-              $handle = fopen($tmppath, "w");
-              fwrite($handle, implode(n", $output));
-
-            /*  Storage::disk('myS3bucket')
-                  ->putFileAs("backups", new File($tmppath), $name);
-            Storage::disk('local')->put('file.txt', 'Contents');
-
-              Storage::disk('local')
-                  ->put($name, $output);
-              fclose($handle);*/
-          //  $command = 'C:\xampp\mysql\bin\mysqldump --opt -h '.$host.' -u '.$username.' -p'.$password.' '.$dbs.' > test2.sql 2>&1';
 
             $filename = 'backups' . date('Y-m-d') . '.sql';
             $allmytables = substr($allmytables1, 1);
@@ -160,25 +88,34 @@ $msg="dump complete";
             $mytables = substr($mytables1, 1);
             $mytable=explode(',',$mytables);
             $mytablestr=str_replace(","," ",$mytables);
-
+//determine if its a full dump or partial dump
             if( $mytables1==""){
                 $alltables= $allmytables;
 
                 $type='Full Dump';
+                //local drive or google drive?
                 if( $dumpsite=='Local Drive'){
-                    echo $dumpsite;
+                    //echo $dumpsite;
                 $command = 'C:\xampp\mysql\bin\mysqldump --opt -h '.$host.' -u '.$username.' -p'.$password.' '.$dbs.' > '.$filename.' 2>&1';
                 }else {
                     $command = 'C:\xampp\mysql\bin\mysqldump --opt -h 34.67.182.226 -P 3306 -u ' . $username . ' -p' . $password . ' ' . $dbs . '--hex-blob --skip-triggers --master-data=1 --order-by-primary --compact --no-autocommit  default-character-set=utf8  --single-transaction --set-gtid-purged=on | gzip |gsutil cp - gs: > Buckets/store_of_stuff 2>&1';
-                    echo $dumpsite;   }
-            }else{
+                    //echo $dumpsite;
+                     }
+            }else{//tables were selected..not a full dump
                 $alltables= $mytables;
                 $type='Partial Dump';//$command='ff';
-                $command = 'C:\xampp\mysql\bin\mysqldump --opt -h '.$host.' -u '.$username.' -p'.$password.' '.$dbs.' '.$mytablestr.' > '.$filename.' 2>&1';
+                if( $dumpsite=='Local Drive'){
+                    //echo $dumpsite;
+                    $command = 'C:\xampp\mysql\bin\mysqldump --opt -h '.$host.' -u '.$username.' -p'.$password.' '.$dbs.' '.$mytablestr.' > '.$filename.' 2>&1';
+                }else {
+                    $command = 'C:\xampp\mysql\bin\mysqldump --opt -h 34.67.182.226 -P 3306 -u ' . $username . ' -p' . $password . ' ' . $dbs .''.$mytablestr. '--hex-blob --skip-triggers --master-data=1 --order-by-primary --compact --no-autocommit  default-character-set=utf8  --single-transaction --set-gtid-purged=on | gzip |gsutil cp - gs: > Buckets/store_of_stuff 2>&1';
+                    //echo $dumpsite;
+                }
+
             }
            exec($command);
            // echo "ret_arr: <br />";
-          print_r($command);
+         // print_r($command);
             //print_r($filename);
            //Insert record in new dumplog table
          $dumplog = new Dumplog;
