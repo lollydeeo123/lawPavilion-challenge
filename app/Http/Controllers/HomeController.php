b2c273496c678b4f1d51d2e754a6c120f38e2230
Lola Ozoh <lollydeeo@yahoo.com>
2020-03-27 02:38:05 +0100

    ammended to request database connection criteria from user

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,11 +2,9 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+
 use Auth;
-use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Schema;
-use PDO;
+
 class HomeController extends Controller
 {
     /**
@@ -26,28 +24,8 @@ class HomeController extends Controller
      */
     public function index()
     {
-        if (Auth::user()){
 
-            $tables = DB::select('SHOW TABLES'); // returns an array of stdObjects
-
-           //$dbh = Schema::getColumnListing($tables);
-
-          $dblist = new PDO('mysql:host=localhost;user=dbuser;password=Db@12345;');
-            $statement = $dblist->query('SHOW DATABASES');
-           $columns= $statement->fetchAll(PDO::FETCH_COLUMN);
-
-
-            //$pdo = new PDO("mysql:host=$host;port=$port", $user, $password);
-
-//Execute a "SHOW DATABASES" SQL query.
-           // $stmt = $pdo->query('SHOW DATABASES');
-
-//Fetch the columns from the returned PDOStatement
-            //$databases = $stmt->fetchAll(PDO::FETCH_COLUMN);
-
-
-        }
-        return view('home',compact('tables','columns'));
+       return view('home');
     }
 
 
